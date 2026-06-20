129a19bc545461592b4a159eeb2979312eafe243
Vitalii Chornyi <vitchorny@gmail.com>
2026-06-20 18:55:36 +0300

    Animate modal window appearance

--- src/components/ui/ModalWindow.tsx
+++ src/components/ui/ModalWindow.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import IconButton from './IconButton';
 import CloseIcon from '@/src/icons/CloseIcon';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface ModalWindowProps {
   children: ReactNode;
@@ -14,21 +15,29 @@ export default function ModalWindow({
   clickOnClose,
 }: ModalWindowProps) {
   return (
-    <div
-      onClick={clickOnClose}
-      className="fixed top-0 left-0 w-screen h-screen bg-overlay flex items-center justify-center"
-    >
-      <div
-        onClick={(e) => e.stopPropagation()}
-        className={`relative m-2 p-6 w-full ${maxWidth} rounded-3xl bg-surface`}
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        onClick={clickOnClose}
+        className="fixed top-0 left-0 w-screen h-screen bg-overlay flex items-center justify-center"
       >
-        <div className="absolute top-6 right-6">
-          <IconButton onClick={clickOnClose}>
-            <CloseIcon />
-          </IconButton>
-        </div>
-        {children}
-      </div>
-    </div>
+        <motion.div
+          initial={{ opacity: 0, y: 50, scale: 0.95 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          exit={{ opacity: 0, y: 50, scale: 0.95 }}
+          onClick={(e) => e.stopPropagation()}
+          className={`relative m-2 p-6 w-full ${maxWidth} rounded-3xl bg-surface`}
+        >
+          <div className="absolute top-6 right-6">
+            <IconButton onClick={clickOnClose}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+          {children}
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
