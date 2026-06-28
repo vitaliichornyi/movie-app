7551ba35f4aede75b96e4134c19eab75649a008b
Vitalii Chornyi <vitchorny@gmail.com>
2026-06-28 11:36:44 +0300

    Add MoviesSkeleton component

---
+++ src/components/skeletons/MoviesSkeleton.tsx
@@ -0,0 +1,13 @@
+export default function MoviesSkeleton() {
+  const placeholders = Array.from({ length: 12 });
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
+      {placeholders.map((placeholder, index) => (
+        <div
+          key={index}
+          className="animate-pulse aspect-[2/3] rounded-2xl bg-surface-container"
+        ></div>
+      ))}
+    </div>
+  );
+}
