af480f5b8dd96716223ea8863766960fa170df67
Vitalii Chornyi <vitchorny@gmail.com>
2026-07-09 16:23:29 +0300

    Fix gradients

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -59,8 +59,8 @@ export default function MoviePage({
                 className="object-cover object-center"
               />
 
-              <div className="absolute inset-x-0 top-0 h-40 bg-gradient-to-b from-black/50 to-transparent" />
-              <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/40 via-30% to-transparent" />
+              <div className="absolute inset-x-0 top-0 h-40 bg-linear-to-b from-black/50 to-transparent" />
+              <div className="absolute inset-0 bg-linear-to-t from-black/60 via-black/40 via-30% to-transparent" />
             </div>
 
             <div className="layout-wrap pb-10">
