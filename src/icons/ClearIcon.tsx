d253da55d4858fa26e3b5812710fa6fd02900a1a
Vitalii Chornyi <vitchorny@gmail.com>
2026-07-16 16:36:35 +0300

    Use currentColor for the icon

--- src/icons/ClearIcon.tsx
+++ src/icons/ClearIcon.tsx
@@ -4,7 +4,7 @@ export default function ClearIcon() {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      fill="none"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
