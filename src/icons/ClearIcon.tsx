9b3d04121b2a448c2a6ce4d0d3b7f52a4ff0c27c
Vitalii Chornyi <vitchorny@gmail.com>
2026-07-20 10:24:30 +0300

    Fix icon issue

--- src/icons/ClearIcon.tsx
+++ src/icons/ClearIcon.tsx
@@ -8,8 +8,8 @@ export default function ClearIcon() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3ZM12 10.8799L9.12012 8L8 9.12012L10.8799 12L8 14.8799L9.12012 16L12 13.1201L14.8799 16L16 14.8799L13.1201 12L16 9.12012L14.8799 8L12 10.8799Z"
         fill="currentColor"
       />
