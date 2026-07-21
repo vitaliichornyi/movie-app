14e8d8d3f53322d44f65b0ab9397a92c5ceb0fbc
Vitalii Chornyi <vitchorny@gmail.com>
2026-07-21 11:53:29 +0300

    Redesign footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,68 +1,102 @@
 import Link from 'next/link';
 
-const footerNav = [
-  {
-    id: 1,
-    title: 'Browse',
-    links: [
-      { id: '1-1', label: 'Popular Movies', href: '/' },
-      { id: '1-2', label: 'New Releases', href: '/' },
-      { id: '1-3', label: 'Animation & Family', href: '/' },
-      { id: '1-4', label: 'Curated Collections', href: '/' },
-    ],
-  },
-  {
-    id: 2,
-    title: 'Shop',
-    links: [
-      { id: '2-1', label: 'Plans & Pricing', href: '/' },
-      { id: '2-2', label: 'Deals & Promo Codes', href: '/' },
-      { id: '2-3', label: 'Gift Cards', href: '/' },
-      { id: '2-4', label: 'Payment Methods', href: '/' },
-    ],
-  },
-  {
-    id: 3,
-    title: 'Support',
-    links: [
-      { id: '3-1', label: 'FAQs', href: '/' },
-      { id: '3-2', label: 'Help Center', href: '/' },
-      { id: '3-3', label: 'Supported Devices', href: '/' },
-    ],
-  },
-  {
-    id: 4,
-    title: 'Company',
-    links: [
-      { id: '4-1', label: 'About Us', href: '/' },
-      { id: '4-2', label: 'Careers', href: '/' },
-      { id: '4-3', label: 'Terms of Service', href: '/' },
-      { id: '4-4', label: 'Privacy Policy', href: '/' },
-    ],
-  },
-];
+import Logo from './ui/Logo';
+import FacebookIcon from '../icons/socialmedia/FacebookIcon';
+import XIcon from '../icons/socialmedia/XIcon';
+import InstagramIcon from '../icons/socialmedia/InstagramIcon';
+import ThreadsIcon from '../icons/socialmedia/ThreadsIcon';
 
 export default function Footer() {
   return (
-    <footer className="layout-wrap grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 pb-32 pt-24">
-      {footerNav.map((item) => (
-        <nav key={item.id} className="flex flex-col gap-2">
-          <h5 className="font-medium">{item.title}</h5>
-          <ul className="flex flex-col gap-1">
-            {item.links.map((link) => (
-              <li key={link.id}>
-                <Link
-                  className="text-sm text-on-surface-variant hover:text-on-surface transition"
-                  href={link.href}
-                  prefetch={false}
-                >
-                  {link.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      ))}
+    <footer className="layout-wrap flex flex-col md:flex-row justify-between items-center gap-6 pb-34 pt-24">
+      <Logo size="sm" />
+      <nav>
+        <ul className="flex gap-6 md:gap-10 text-sm font-medium tracking-wide text-on-surface-variant">
+          <li>
+            <Link
+              className="hover:text-on-surface transition"
+              href="/"
+              prefetch={false}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-on-surface transition"
+              href="/"
+              prefetch={false}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-on-surface transition"
+              href="/"
+              prefetch={false}
+            >
+              Pricing
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-on-surface transition"
+              href="/"
+              prefetch={false}
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-on-surface transition"
+              href="/"
+              prefetch={false}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <ul className="flex gap-3 pt-1 text-on-surface-variant">
+        <li>
+          <Link
+            className="hover:text-on-surface transition"
+            prefetch={false}
+            href="https://www.facebook.com/"
+          >
+            <FacebookIcon />
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="hover:text-on-surface transition"
+            prefetch={false}
+            href="https://x.com/"
+          >
+            <XIcon />
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="hover:text-on-surface transition"
+            prefetch={false}
+            href="https://www.instagram.com/"
+          >
+            <InstagramIcon />
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="hover:text-on-surface transition"
+            prefetch={false}
+            href="https://www.threads.com/"
+          >
+            <ThreadsIcon />
+          </Link>
+        </li>
+      </ul>
     </footer>
   );
 }
--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Logo() {
+interface LogoProps {
+  size?: 'sm' | 'md';
+}
+
+export default function Logo({ size = 'md' }: LogoProps) {
   return (
     <Link href="/">
       <Image
         src="/logo.svg"
         alt="Movie logo"
-        width={122}
+        width={size === 'md' ? 128 : size === 'sm' ? '110' : '124'}
         height={34}
         priority
       />
